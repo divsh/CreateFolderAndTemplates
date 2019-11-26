fb17c3b7827d0b499dc37b4ded87e12d718ef9e0
divsh <divsh23@gmail.com>
2019-11-26 17:38:34 +1300

    unfinished

--- CreateEmail/Form1.vb
+++ CreateEmail/Form1.vb
@@ -3,31 +3,45 @@ Imports Microsoft.Office.Interop.Outlook
 
 Public Class Form1
     Private Sub btnCreateEmail_Click(sender As Object, e As EventArgs) Handles btnCreateEmail.Click
-
+        InsertBodyTextInOutlookWordEditor("C:\A1\work\autoDoc\259111 P2 - Linux machine disk space issue.docx")
     End Sub
 
     Sub CreateMail()
         Dim filePath As String
         filePath = """C:\\Users\\Me\\Desktop\\test.docx"""
-        InsertBodyTextInOutlookWordEditor filePath
-End Sub
+        InsertBodyTextInOutlookWordEditor(filePath)
+    End Sub
 
     Sub InsertBodyTextInOutlookWordEditor(filePath As String)
         Dim myMail As Outlook.MailItem
         Dim myInspector As Outlook.Inspector
         Dim wdDoc As Word.Document
         Dim wdRange As Word.Range
+        Dim app As Outlook.Application = New Outlook.Application()
 
+        wdDoc = (New Word.Application).Documents.Open(FileName:=filePath, [ReadOnly]:=True)
         On Error Resume Next
-        myMail = Application.CreateItem(OlItemType.olMailItem)
+        myMail = app.CreateItem(OlItemType.olMailItem)
         myMail.Subject = "Here's the latest..."
-        myMail.Display
+        myMail.Display()
         myInspector = myMail.GetInspector
         wdDoc = myInspector.WordEditor
         If Not (wdDoc Is Nothing) Then
-            wdRange = wdDoc.Range(0, wdDoc.Characters.Count)
-            wdRange.Fields.Add(Range:=wdRange, Type:=wdFieldEmpty, Text:="INCLUDETEXT  " & filePath, PreserveFormatting:=True)
+            wdRange = wdDoc.Content
+            wdRange.Select()
+            wdRange.Copy()
+
+            ' myMail.HTMLBody = wdRange.Text
+            wdRange.PasteSpecial()
+            wdDoc.Close()
+
+            'wdRange = wdDoc.Range(0, wdDoc.Characters.Count)
+            'wdRange.Fields.Add(Range:=wdRange, Type:=Word.WdFieldType.wdFieldEmpty, Text:="INCLUDETEXT  " & filePath, PreserveFormatting:=True)
+            'wdRange.Fields.Add(Range:=wdRange, Type:=Word.WdFieldType.wdFieldEmpty, Text:=filePath, PreserveFormatting:=True)
         End If
+
     End Sub
+
+
 
 End Class
